99540667b7a1ef692329ad23623e1bfad25cb3a8
Corinna Dommes <cdommes@stud.hs-bremen.de>
2024-06-13 15:02:27 +0200

    CSS fixes, 3rd Dashboard

--- src/views/DashboardKennzahlen.js
+++ src/views/DashboardKennzahlen.js
@@ -43,17 +43,17 @@ function DashboardKennzahlen() {
               <Card.Body>
                 <Row>
                   <Col xs="4">
-                    <div className="icon-big text-center icon-warning">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_BLAU} class="bi bi-arrow-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                  <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_TUERKIS} class="bi bi-people-fill" viewBox="0 0 16 16">
+                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
                       </svg>
                     </div>
                   </Col>
                   <Col xs="8">
                     <div className="numbers">
-                      <p className="card-category">Ø Schwundfaktor</p>
+                      <p className="card-category">Studierende in RSZ</p>
                       {/* <Card.Title as="h3">{Math.round(avg(schwundfaktorDaten[6].faktor)*10000,4)/10000}</Card.Title> */}
-                      <Card.Title as="h3">1,1928</Card.Title>
+                      <Card.Title as="h3">100 - 90%</Card.Title>
                     </div>
                   </Col>
                 </Row>
@@ -61,6 +61,29 @@ function DashboardKennzahlen() {
             </Card>
           </Col>
           <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_GRUEN} class="bi bi-people-fill" viewBox="0 0 16 16">
+                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">Studierende in RSZ+2</p>
+                      <Card.Title as="h3">111 - 100%</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        <Col lg="3" sm="3">
             <Card>
               <Card.Body>
                 <Row>
@@ -76,6 +99,118 @@ function DashboardKennzahlen() {
                     <div className="numbers">
                       <p className="card-category">Studieanfänger:innen WiSe 23/24</p>
                       <Card.Title as="h3">35</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_TUERKIS} class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">SAP <br></br>WiSe 23/24</p>
+                      <Card.Title as="h3">40</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_GRUEN} class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">Besetzte SAP <br></br> WiSe 23/24</p>
+                      <Card.Title as="h3">87,5%</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_BLAU} class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                        <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                        <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">Absolvierende <br></br>SoSe 23</p>
+                      <Card.Title as="h3">25</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_TUERKIS} class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                        <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                        <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">Abbrechende <br></br>SoSe 23</p>
+                      <Card.Title as="h3">5</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" sm="3">
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col xs="4">
+                    <div className="icon-big text-center icon-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={RGB_CD_GRUEN} class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                        <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                        <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+                      </svg>
+                    </div>
+                  </Col>
+                  <Col xs="8">
+                    <div className="numbers">
+                      <p className="card-category">Erfolgsquote <br></br> SoSe 23</p>
+                      <Card.Title as="h3">83,3%</Card.Title>
                     </div>
                   </Col>
                 </Row>
