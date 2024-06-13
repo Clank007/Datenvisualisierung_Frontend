d04dcb0183e30ed858889763db1a787d99bc8556
Fabian <fabiandirscherl@web.de>
2024-06-13 14:34:12 +0200

    Create FAQDictionary.tsx

---
+++ src/assets/FAQDictionary.tsx
@@ -0,0 +1,10 @@
+// Key Value Pair
+const FAQDictionary = {
+    dataIncomplete: {
+        name:   'Unvollständige Daten',
+        short:  'Mind. 1 ausgewählter Studiengang hat unzureichende Daten',
+        long:   'Bei der Auswahl der Daten hat mindestens ein Studiengang nicht genug Datensätze für alle Jahre.',
+    },
+}
+
+export default FAQDictionary;
