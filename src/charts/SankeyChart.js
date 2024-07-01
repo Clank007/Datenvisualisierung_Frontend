3b5508a242a4db4f676153392521a786a7924a0d
Corinna Dommes <cdommes@stud.hs-bremen.de>
2024-07-01 11:24:18 +0200

    API added to Studienverlauf

--- src/charts/SankeyChart.js
+++ src/charts/SankeyChart.js
@@ -1,23 +1,76 @@
 import { Chart } from 'react-google-charts';
+import { fetchStudyProgressAnalysis } from '../util/api_calls';
+import { useEffect, useState } from 'react';
 
-const SankeyChart = () => {
+// Function to filter based on course and year
+function filterByCourseAndYear(data, targetCourse, targetYear) {
+  return data.filter(item => {
+    // Extract the year number using a regular expression
+    const yearMatch = item.year.match(/\d{4}/);
+    const year = yearMatch ? yearMatch[0] : null;
+    if (item.course === targetCourse && year === targetYear) console.log(item.course + targetCourse + year + targetYear);
+    return item.course === targetCourse && year === targetYear;
+  });
+}
+
+const SankeyChart = (props) => {
+  console.log(props);
   const colors = ['#32B4C8','#32B4C8', '#32B4C8', '#32B4C8', '#32B4C8', '#32B4C8', '#32B4C8', '#32B4C8', '#32B4C8', '#6FA53C'];
   
-  const chartData = [
-    ['From', 'To', 'Weight'],
-    ['1. FS', '2. FS', 26],
-    ['2. FS', '3. FS', 25],
-    ['3. FS', '4. FS', 23],
-    ['4. FS', '5. FS', 21],
-    ['5. FS', '6. FS', 21],
-    ['6. FS', '7. FS', 21],
-    ['7. FS', '8. FS', 20],
-    ['8. FS', 'Absolventen', 19],
-    ['2. FS', 'Fachwechsel oder Studienabbruch', 1],
-    ['3. FS', 'Fachwechsel oder Studienabbruch', 2],
-    ['4. FS', 'Fachwechsel oder Studienabbruch', 2],
-    ['7. FS', 'Fachwechsel oder Studienabbruch', 1],
-  ];
+  const [studyProgressAnalysis, setStudyProgressAnalysis] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const standardPeriodOfStudyResult = await fetchStudyProgressAnalysis();
+        setStudyProgressAnalysis(standardPeriodOfStudyResult);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const chartData = () => {
+    if (studyProgressAnalysis != undefined) {
+      const correctedYear = (parseInt(props.selectedYear.year)-4).toString();
+      const cohort = filterByCourseAndYear(studyProgressAnalysis, props.selectedYear.course, correctedYear)[0].cohorts;
+      console.log(cohort);
+      var lastCount = studyProgressAnalysis[1].cohorts[0];
+      var transitions = [];
+      var leavers = [];
+      
+      for (var i = 0; i < (cohort.length-1); i++) {
+        const studentCount = cohort[i];
+        transitions.push([i+1 + '. FS', i+2 + '. FS', studentCount]);
+        if (studentCount < lastCount) {
+          leavers.push([i+1 + '. FS', 'Fachwechsel oder Studienabbruch', lastCount-studentCount]);
+        }
+        lastCount = studentCount;
+      }
+
+      transitions.push([cohort.length + '. FS', 'Absolventen', cohort[cohort.length-1]]);
+
+      return [
+        ['From', 'To', 'Weight'],
+        ...transitions,
+        ...leavers,
+      ];
+    }
+  }
 
   const chartOptions = {
     sankey: {
@@ -47,7 +100,7 @@ const SankeyChart = () => {
         height={'400px'}
         chartType="Sankey"
         loader={<div>Loading Chart</div>}
-        data={chartData}
+        data={chartData()}
         options={chartOptions}
         rootProps={{ 'data-testid': '1' }}
       />
--- src/views/DashboardStudienverlauf.js
+++ src/views/DashboardStudienverlauf.js
@@ -7,19 +7,17 @@ import {
 } from "react-bootstrap";
 import SankeyChart from "../charts/SankeyChart";
 
-const sum = (X) => X.reduce((a, b) => a + b, 0);
-const avg = (X) => (sum(X) / X.length) || 0;
 
-function DashboardStudienverlauf() {
+function DashboardStudienverlauf(props) {
   return (
     <>
       <Container fluid>
         <Row>
           <Col xs="10">
             <Card className="card-chart">
-              <Card.Title as="h3" className="text-center pt-4">Studienverlauf DSI WiSe 17/18</Card.Title>
+              <Card.Title as="h3" className="text-center pt-4">Studienverlauf {props.selectedYear.course} Kohorte {parseInt(props.selectedYear.year)-4}</Card.Title>
               <Card.Body>
-                <SankeyChart></SankeyChart>
+                <SankeyChart selectedYear={props.selectedYear} selectedBaseCourse={props.selectedBaseCourse}></SankeyChart>
               </Card.Body>
             </Card>
           </Col>
